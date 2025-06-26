72b9ba956a9b1fb9373861b83232db521d3d4884
Martha488 <moralesm@brynmawrschool.org>
2025-06-26 08:17:52 -0400

    Add files via upload

---
+++ 6-24 functions.py
@@ -0,0 +1,8 @@
+#syntax: ded<name of your function>(optional: inputs)
+
+def hello():
+    print("hello world")
+
+hello()
+print(hello)
+print(hello()) #an extra print outside the funciton itself will give weird behavior
---
+++ 6-25.py
@@ -0,0 +1,32 @@
+"""
+def exaple(num): 
+    print(num +1)
+
+exaple(5) #passing something into a function
+
+def example2(num):
+    num = num +10
+
+number = 15
+print(number)
+example2(number)
+print(number)
+
+def example3(mylist):
+    mylist.append("hello from example3")
+
+strings = ("Hello", "World")
+print(strings)
+example3(strings)
+print(strings)
+
+Things that get passed in as value:
+    - integers
+    - floating point numbers
+    - booleans (true or false)
+
+Things that get passed as its reference (immutable)
+    - lists
+    - dictionaries
+"""""
+
